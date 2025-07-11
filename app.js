3e15982701172071853060cf4c19545da23228cf
mrlancecgm <malaluan.lancechester@gmail.com>
2025-07-12 00:53:32 +0800

    electron

---
+++ app.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+
+let appWindow;
+
+function createWindow() {
+  appWindow = new BrowserWindow({
+    fullscreen: true,
+    autoHideMenuBar: true,
+  });
+  appWindow.loadFile("dist/rainbow-rumble-app/browser/index.html");
+
+  appWindow.on("closed", function () {
+    appWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
